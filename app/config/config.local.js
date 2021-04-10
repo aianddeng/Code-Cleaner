28aa21a49fdcd01405d972325afc0457bca433d3
Chase <aianddeng@gmail.com>
2021-04-10 13:49:52 +0800

    use production extension

--- app/config/config.local.js
+++ app/config/config.local.js
@@ -4,10 +4,7 @@ module.exports = {
     concurrency: 1,
     headless: false,
     timeout: 1 * 60 * 1000,
-    extensionPath:
-        process.env.NODE_ENV === 'production'
-            ? path.join(__dirname, '../../', 'dist')
-            : '~/Work-Base/fatcoupon-xtension/dist/extension-production-local',
+    extensionPath: path.join(__dirname, '../../', 'dist'),
     mongoPath:
         process.env.NODE_ENV === 'production'
             ? 'mongodb://chase:372100@us.forden.cn:27017/product_code_task?authSource=admin'
