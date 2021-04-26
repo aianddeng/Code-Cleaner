2cbcdf288a430133b1165d007cd584b7181157c2
Chase <aianddeng@gmail.com>
2021-04-26 09:11:22 +0800

    fix bloomingdales

--- src/app/chrome/mappings/5e900bf297ae8d00118d9481.js
+++ src/app/chrome/mappings/5e900bf297ae8d00118d9481.js
@@ -1,14 +1,7 @@
 module.exports = {
   storeId: '5e900bf297ae8d00118d9481',
+  product:
+    'https://www.bloomingdales.com/shop/product/michael-aram-orchid-throw?ID=3983048',
+  button: 'button.add-to-bag',
   cart: 'https://www.bloomingdales.com/my-bag',
-  login: {
-    url: 'https://www.bloomingdales.com/account/signin',
-    username: 'deng@fatcoupon.com',
-    password: 'dengfatcouponcom',
-    selector: {
-      username: '#email',
-      password: '#pw-input',
-      button: '#sign-in',
-    },
-  },
 }
