3f2fdae02a312ba71dd44a60e5c9e1e71fd7a976
Chase <aianddeng@gmail.com>
2021-04-26 18:05:31 +0800

    update

--- src/app/jobs/queue.js
+++ src/app/jobs/queue.js
@@ -5,24 +5,20 @@ const Settings = require('../models/Settings')
 
 const queue = new Queue('fatcoupon', redis.client)
 
-queue.process(
-  'clean-code',
-  process.env.NODE_ENV === 'production' ? 3 : 1,
-  async (job, done) => {
-    const { storeId, coupons } = job.data
+queue.process('clean-code', 1, async (job, done) => {
+  const { storeId, coupons } = job.data
 
-    const [settings] = await Settings.find({}).sort({ _id: -1 }).limit(1)
+  const [settings] = await Settings.find({}).sort({ _id: -1 }).limit(1)
 
-    const unTestCoupon = coupons
-      .filter((el) => {
-        if (!settings || settings.promoType === 'all') return true
-        return el.type === settings.promoType
-      })
-      .filter((el) => !el.validStatus)
+  const unTestCoupon = coupons
+    .filter((el) => {
+      if (!settings || settings.promoType === 'all') return true
+      return el.type === settings.promoType
+    })
+    .filter((el) => !el.validStatus)
 
-    unTestCoupon.length ? run(storeId, unTestCoupon, job, done) : done()
-  }
-)
+  unTestCoupon.length ? run(storeId, unTestCoupon, job, done) : done()
+})
 
 queue.on('waiting', async (jobId) => {
   const job = await queue.getJob(jobId)
