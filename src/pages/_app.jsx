3ff0acf9b728bc75c287cadafdf84dc6079fa181
Chase <aianddeng@gmail.com>
2021-05-10 18:23:00 +0800

    update new theme

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,29 +1,30 @@
 import Wrapper from '@comp/Wrapper'
 import dynamic from 'next/dynamic'
 import axios from 'axios'
+import '../style/global.css'
 
-const Theme = dynamic(async () => {
-  const { data } = await axios.get('/api/settings')
+// const Theme = dynamic(async () => {
+//   const { data } = await axios.get('/api/settings')
 
-  if (data.themeType === 'dark') {
-    await import('antd/dist/antd.dark.min.css')
-    if (globalThis.document) {
-      const html = document.querySelector('html')
-      html.classList.add('dark')
-    }
-  } else {
-    await import('antd/dist/antd.min.css')
-    if (globalThis.document) {
-      const html = document.querySelector('html')
-      html.classList.remove('dark')
-    }
-  }
-  await import('../style/global.css')
-})
+//   if (data.themeType === 'dark') {
+//     await import('antd/dist/antd.dark.min.css')
+//     if (globalThis.document) {
+//       const html = document.querySelector('html')
+//       html.classList.add('dark')
+//     }
+//   } else {
+//     await import('antd/dist/antd.min.css')
+//     if (globalThis.document) {
+//       const html = document.querySelector('html')
+//       html.classList.remove('dark')
+//     }
+//   }
+//   await import('../style/global.css')
+// })
 
 const MyApp = ({ Component, pageProps }) => (
   <Wrapper>
-    <Theme />
+    {/* <Theme /> */}
     <Component {...pageProps} />
   </Wrapper>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
   purge: ['./src/pages/**/*.jsx', './src/components/**/*.jsx'],
-  darkMode: 'class',
+  darkMode: 'media',
   theme: {
     extend: {},
   },
