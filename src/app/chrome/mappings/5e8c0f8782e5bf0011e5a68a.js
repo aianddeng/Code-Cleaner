3f62a33b4680f83f6a65a2f112fbad73a64c6298
Chase <aianddeng@gmail.com>
2021-05-13 18:25:43 +0800

    small fix mappings

--- src/app/chrome/mappings/5e8c0f8782e5bf0011e5a68a.js
+++ src/app/chrome/mappings/5e8c0f8782e5bf0011e5a68a.js
@@ -1,6 +1,7 @@
 module.exports = {
   storeId: '5e8c0f8782e5bf0011e5a68a',
-  product: 'https://store.acer.com/en-us/nitro-5-gaming-laptop-an515-44-r0dl',
+  product:
+    'https://store.acer.com/en-us/predator-helios-300-gaming-laptop-ph315-53-71qx',
   button: '#product-addtocart-button',
   cart: 'https://store.acer.com/en-us/checkout/cart/',
 }
--- src/app/chrome/mappings/5ed0b7f057659f0011be1b4e.js
+++ src/app/chrome/mappings/5ed0b7f057659f0011be1b4e.js
@@ -1,5 +1,5 @@
 module.exports = {
-  storeId: '5ed0b7f057659f0011be1b50',
+  storeId: '5ed0b7f057659f0011be1b4e',
   product:
     'https://www.meshki.us/collections/accessories/products/arletta-dog-hoodie-black',
   button: ['.btn--add-to-cart'],
