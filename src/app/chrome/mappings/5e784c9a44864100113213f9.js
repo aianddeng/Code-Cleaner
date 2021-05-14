9466336b937a1b39cf199bb149d30834afd95d90
Chase <aianddeng@gmail.com>
2021-05-14 10:04:05 +0800

    mappings update

--- src/app/chrome/mappings/5e784c9a44864100113213f9.js
+++ src/app/chrome/mappings/5e784c9a44864100113213f9.js
@@ -1,6 +1,9 @@
 module.exports = {
   storeId: '5e784c9a44864100113213f9',
-  product: 'https://deals.dell.com/en-us/mpp/productdetail/8kzq',
-  button: '#affix-right-rail button.get-deal',
+  product: 'https://deals.dell.com/en-us/productdetail/8roq',
+  button: [
+    '#affix-right-rail button.get-deal',
+    '.accessories .btn + a.modal-proceed',
+  ],
   cart: 'https://www.dell.com/en-us/buy',
 }
--- src/app/chrome/mappings/5ed0b7f057659f0011be1b4e.js
+++ src/app/chrome/mappings/5ed0b7f057659f0011be1b4e.js
@@ -1,7 +1,7 @@
 module.exports = {
   storeId: '5ed0b7f057659f0011be1b4e',
   product:
-    'https://www.meshki.us/collections/accessories/products/arletta-dog-hoodie-black',
+    'https://www.meshki.us/collections/all-clothing/products/elena-fitted-blazer-chocolate',
   button: ['.btn--add-to-cart'],
   cart: 'https://www.meshki.us/checkout',
 }
