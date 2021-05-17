380f893d0b3d9abd293271436f32a439c3038810
Chase <aianddeng@gmail.com>
2021-05-17 17:57:06 +0800

    update mappings

--- src/app/chrome/mappings/5e900c9d97ae8d00118d9482.js
+++ src/app/chrome/mappings/5e900c9d97ae8d00118d9482.js
@@ -1,7 +1,7 @@
 module.exports = {
   storeId: '5e900c9d97ae8d00118d9482',
   product:
-    'https://www.bobbibrowncosmetics.com/product/14358/7485/Best-Sellers/Vitamin-Enriched-Face-Base/VEFB',
+    'https://www.bobbibrowncosmetics.com/product/14008/77812/skincare/eye-care/vitamin-enriched-eye-base/fh20',
   button: ['.product-full__bag-btn .product__button--add-to-bag'],
   cart: 'https://www.bobbibrowncosmetics.com/checkout/viewcart.tmpl',
 }
---
+++ src/app/chrome/mappings/5ed0b7f057659f0011be1b60.js
@@ -0,0 +1,8 @@
+module.exports = {
+  storeId: '5ed0b7f057659f0011be1b60',
+  product:
+    'https://www.rag-bone.com/mens/accessories/addison-baseball-cap-WJW21S1013JA13.html',
+  button: ['#add-to-cart'],
+  cart: 'https://www.rag-bone.com/cart',
+  useLocalScript: true,
+}
