5a5f7ef56d4b8f88e991f0b2addcbe7bd79bf937
Chase <aianddeng@gmail.com>
2021-05-18 13:59:56 +0800

    update mappings

--- src/app/chrome/mappings/5e72e0c52b5d0300113183b2.js
+++ src/app/chrome/mappings/5e72e0c52b5d0300113183b2.js
@@ -1,6 +1,6 @@
 module.exports = {
   storeId: '5e72e0c52b5d0300113183b2',
-  product: 'https://www.newbalance.com/pd/champ-backpack/LAB91012.html',
+  product: 'https://www.newbalance.com/pd/all-terrain-backpack/LAB11120.html',
   button: '#productAtributes .add-to-cart',
   cart: 'https://www.newbalance.com/cart/',
 }
--- src/app/chrome/mappings/5e913b5ddfc61600114f4d80.js
+++ src/app/chrome/mappings/5e913b5ddfc61600114f4d80.js
@@ -1,8 +1,7 @@
 module.exports = {
   storeId: '5e913b5ddfc61600114f4d80',
   product:
-    'https://www.pacsun.com/on/demandware.store/Sites-pacsun-Site/default/Product-Variation?pid=0685601770010&dwvar_0685601770010_size=9900&dwvar_0685601770010_color=001&source=detail&uuid=',
+    'https://www.pacsun.com/playboy/by-pacsun-x-croakies-retainer-0685601770010.html?dwvar_0685601770010_color=001&dwvar_0685601770010_size=9900',
   button: ['#va-sizeCode li:first-of-type a', '#addToCartBtn'],
-  cart:
-    'https://www.pacsun.com/on/demandware.store/Sites-pacsun-Site/default/Cart-Show',
+  cart: 'https://www.pacsun.com/on/demandware.store/Sites-pacsun-Site/default/Cart-Show',
 }
