07792ea48167f184119a4c61156a3b73a4a98ac9
Chase <aianddeng@gmail.com>
2021-05-25 17:17:48 +0800

    add new mapping

---
+++ src/app/chrome/mappings/5ef16d7df627230011e687e5.js
@@ -0,0 +1,7 @@
+module.exports = {
+  storeId: '5ef16d7df627230011e687e5',
+  product:
+    'https://www.marcjacobs.com/default/the-softshot-21/191267899381.html',
+  button: ['.pdpw__button-add--button'],
+  cart: 'https://www.marcjacobs.com/default/checkout',
+}
