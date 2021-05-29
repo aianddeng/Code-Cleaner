6f414f9e241fbfa1f0566ac17e38b9d9062ecbc6
Chase <aianddeng@gmail.com>
2021-05-29 17:10:26 +0800

    update mappings.

---
+++ src/app/chrome/mappings/5ee30c6bd883e900117e3cc1.js
@@ -0,0 +1,7 @@
+module.exports = {
+  storeId: '5ee30c6bd883e900117e3cc1',
+  product:
+    'https://uspoloassn.com/collections/mens-polo-shirts/products/uspa-flag-vertical-stripe-polo-shirt',
+  button: ['.button--add-to-cart[name=add]'],
+  cart: 'https://uspoloassn.com/checkout',
+}
