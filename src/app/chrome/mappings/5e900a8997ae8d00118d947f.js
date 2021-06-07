f41d73e459d9d88dcd715607e86090d4993423b7
Chase <aianddeng@gmail.com>
2021-06-07 18:19:16 +0800

    add mappings.

---
+++ src/app/chrome/mappings/5e900a8997ae8d00118d947f.js
@@ -0,0 +1,8 @@
+module.exports = {
+  storeId: '5e900a8997ae8d00118d947f',
+  product:
+    'https://www.belk.com/p/greg-norman-collection-short-sleeve-pro-tek-micro-polo-shirt/3201501G7DSK440.html?dwvar_3201501G7DSK440_color=454286345871',
+  button: ['#va-size', '#add-to-cart'],
+  cart: 'https://www.belk.com/shopping-bag/',
+  useLocalScript: true,
+}
---
+++ src/app/chrome/mappings/5e9e88a513920f0011cdfa29.js
@@ -0,0 +1,14 @@
+module.exports = {
+  storeId: '5e9e88a513920f0011cdfa29',
+  login: {
+    url: 'https://login.aliexpress.com',
+    username: 'deng@fatcoupon.com',
+    password: 'MOchen110',
+    selector: {
+      username: '#fm-login-id',
+      password: '#fm-login-password',
+      button: '.fm-button',
+    },
+  },
+  cart: 'https://shoppingcart.aliexpress.com/orders.htm?aeOrderFrom=main_shopcart&availableProductShopcartIds=81007924883660',
+}
