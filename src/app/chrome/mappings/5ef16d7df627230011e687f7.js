983786e791e776e230972a3c02bfbb424baff1d7
Chase <aianddeng@gmail.com>
2021-06-11 15:36:10 +0800

    add mappings.

---
+++ src/app/chrome/mappings/5ef16d7df627230011e687f7.js
@@ -0,0 +1,7 @@
+module.exports = {
+  storeId: '5ef16d7df627230011e687f7',
+  product:
+    'https://www.theoutnet.com/en-us/shop/product/stella-mccartney/dresses/knee-length-dress/printed-cape-effect-satin-crepe-dress/19971654706852484',
+  button: ['[name=sizeSelect]', '.primaryButton[aria-label*=Bag]'],
+  cart: 'https://www.theoutnet.com/en-us/checkout',
+}
---
+++ src/app/chrome/mappings/Error_5ea01761c571ab001124bf71.js
@@ -0,0 +1,9 @@
+module.exports = {
+  storeId: '5ea01761c571ab001124bf71',
+  product:
+    'https://www.vitaminshoppe.com/p/n-o-xplode-vaso-cherry-bomb-1-11-lb-powder/ip-1116',
+  button: ['.addToCart-fix-circle-loader'],
+  cart: 'https://www.vitaminshoppe.com/s/myAccount/login.jsp',
+  useLocalScript: true,
+}
+// the server has some error
