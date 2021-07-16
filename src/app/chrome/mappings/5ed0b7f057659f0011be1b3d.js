27aa78f7246c85de66db2b3854c349f8785fef28
Chase <aianddeng@gmail.com>
2021-07-16 18:14:27 +0800

    add dockers

---
+++ src/app/chrome/mappings/5ed0b7f057659f0011be1b3d.js
@@ -0,0 +1,22 @@
+module.exports = {
+  storeId: '5ed0b7f057659f0011be1b3d',
+  product:
+    'https://www.dockers.com/US/en_US/accessories/men/polarized-clubmaster-sunglasses/p/M24882001',
+  button: [
+    '.product-main-info .tiles-outer-grid-container>div:first-of-type button:not(.unavailable)',
+    'undefind',
+    'undefind',
+    'undefind',
+    'undefind',
+    'undefind',
+    '.product-main-info .tiles-outer-grid-container>div:last-of-type button:not(.unavailable)',
+    'undefind',
+    'undefind',
+    'undefind',
+    'undefind',
+    'undefind',
+    '#addToCartButton',
+  ],
+  cart: 'https://www.dockers.com/US/en_US/cart',
+  useLocalScript: true,
+}
