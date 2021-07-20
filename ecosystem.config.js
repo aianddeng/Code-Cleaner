7a193fc44d7d3287bc294ef959e6c09230854e64
Chase <aianddeng@gmail.com>
2021-07-20 18:06:01 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'FatCoupon-Cleaner',
-      script: './src/server.js',
+      script: 'src/server.js',
       env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
         NODE_ENV: 'production',
       },
-      cron_restart: '0 0 23 * * ?',
+      cron_restart: '0 0 1/12 * *',
     },
   ],
 }
