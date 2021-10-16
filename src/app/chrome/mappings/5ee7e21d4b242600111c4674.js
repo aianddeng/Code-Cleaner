2092b92ef5e30fd4e2dbefd8156b8e7d4e211d3e
deng <aianddeng@gmail.com>
2021-10-16 09:45:01 +0800

    update coachoutlet

--- src/app/chrome/mappings/5ee7e21d4b242600111c4674.js
+++ src/app/chrome/mappings/5ee7e21d4b242600111c4674.js
@@ -1,5 +1,4 @@
 module.exports = {
-  unRequestInterception: true,
   storeId: '5ee7e21d4b242600111c4674',
   product:
     'https://www.coachoutlet.com/products/graham-pack-in-signature-canvas/C2932.html?dwvar_color=QBMI5',
